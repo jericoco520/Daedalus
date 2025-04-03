661f43b07c8818c9f7644d22efecaf57cd9a7845
Jerico Corneja <jerico.corneja20@gmail.com>
2025-04-03 08:54:09 -0400

    rework the piping and addresses

--- Transmission/sender04.py
+++ Transmission/sender04.py
@@ -1,7 +1,7 @@
 import time
 from pyrf24 import RF24 , RF24_PA_LOW, RF24_DRIVER, RF24_2MBPS   # Module for NRF24L01
 
-radio = RF24(22, 0)  # CE=GPIO22, CSN=SPI0 CS0
+radio = RF24(22, 0)  # CE = GPIO22, CSN = CE0 on SPI bus 0: /dev/spidev0.0 
 
 # Set up radio
 def setup():
@@ -10,12 +10,15 @@ def setup():
         print("Radio hardware not responding")
         return
 
+    address = [b"1Node", b"2Node"]       # Addresses for communication
+    radio.open_tx_pipe(address[0])       # Open TX pipe with address[0]
+    radio.open_rx_pipe(1, address[1])    # Open RX pipe with address[1]
     # Set radio parameters
-    radio.setChannel(0x76)               # Set channel to 0x76
+    #radio.setChannel(0x76)               # Set channel to 0x76
     radio.setPALevel(RF24_PA_LOW)        # Power Amplifier level
     radio.setDataRate(RF24_2MBPS)        # Data rate
     radio.set_auto_ack(True)             # Enable auto acknowledgment
-    radio.openWritingPipe(b'1Node')      # Address to send to
+    #radio.openWritingPipe(b'1Node')      # Address to send to
     radio.printPrettyDetails()           # Print radio details
     radio.listen = False                 # Set radio in TX mode
     
